220548cc303387a8bd3a7bbc5f03f0ee4701e97c
Jerome Lacote <mail@jeromelacote.com>
2022-09-05 21:54:04 -0700

    import

--- ImmerFilm/Controllers/Notifications/NotificationPopoverPresentationControllerDelegate.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class NotificationPopoverPresentationControllerDelegate : UIViewController, UIPopoverPresentationControllerDelegate {
-
-    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-        return UIModalPresentationStyle.none
-    }
-            
-}
---
+++ ImmerFilm/ImmerFilm/ImmerFilm/ImmerFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmerFilmApp.swift
+//  ImmerFilm
+//
+//  Created by Trevor Welsh on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ImmerFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
